b6358ed77111cb9d255e001b919caa41b22971d1
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-03-04 11:40:15 +0100

    add Navbar links with onclick active link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,6 +32,21 @@ const Navbar = () => {
             Niclas | Portfolio
           </p>
         </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title
+                  ? 'text-white'
+                  : 'text-secondary'
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   );
