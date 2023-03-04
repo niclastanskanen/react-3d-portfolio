7c50000cc23218620b47c078037602e8025d2809
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-03-04 15:58:42 +0100

    add background to Hero compoenent and gradient line

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
             alt="logo"
             className="w-9 h-9 object-contain"
           />
-          <p className="text-white text-[18px] font-bold cursor-pointer">
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
             Niclas | Portfolio
           </p>
         </Link>
